553d9b8628ff7b4930b6e19415494f18999bf115
ainiatiqaha <atiqahaini13@gmail.com>
2024-11-11 01:24:17 +0800

    Modify existing file and add new file for product management

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Products extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'SKU', 'base_price', 'specifications', 'category', 'industry_params'];
+
+    public function inventory()
+    {
+        return $this->hasMany(Inventory::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
+
+?>
