6af844292ab9531be6276329588489aaac27796f
Ahmad Fitri <ahmadfitriyusof1997@gmail.com>
2024-11-10 18:25:05 +0800

    Initial commit

---
+++ database/migrations/2024_11_10_080720_create_inventory_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('warehouse_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->date('expiry_date')->nullable();
+            $table->string('batch_number');
+            $table->json('tracking_data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory');
+    }
+};
